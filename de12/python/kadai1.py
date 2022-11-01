7d8d04956465e9b97712622ccb7e7c3298db25b6
KohakuKomatsu <r202201791yk@jindai.jp>
2022-11-01 14:55:01 +0900

    20221101

---
+++ de12/python/kadai1.py
@@ -0,0 +1,39 @@
+print("今日は剣道の全国大会だ！神奈川大学は運よく決勝戦に進出することができた。あなたは大将前3人は引き分け。緊張の中堅戦が今始まる")
+name=input("大将であるあなたの苗字は？")
+
+selecta = int(input("数字の1から3を選べ"))
+if selecta ==1:
+print("中堅が初太刀から面を打ちに行き鮮やかな返し胴を打たれてしまったが、直後に小手を返し、そのまた直後に相小手面を返す")
+
+elif selecta == 3:
+print("特に良い展開もなかったが、後半に引き面を取る")
+
+elif selecta == 2:
+print("慎重に試合を進め、後半に突きを取る")
+
+print("続く5将は一本負け。さあ副将戦どうなる⁉")
+selectb = int(input("数字の1から3を選べ"))
+if selectb == 1: 
+print("特に何もなく引き分け")
+
+elif selectb == 2:
+print("")
+
+elif selectb == 3:
+print("）
+print/“"
+"やはり遅刻になった。そのとき、あなたは")
+
+selectc = int(input（！")）
+if selectc == 1:
+print"
+”. ")
+
+elif selecte == 2:
+print(）
+
+elif selecte ==3:
+print()
+
+
+
---
+++ de12/python/work1.py
@@ -0,0 +1,11 @@
+from turtle import window_height
+
+
+name="瑚舶"
+waist=77
+age=18
+height=175.5
+weight=67
+
+print(name, "さんは腹囲", waist, "cmで年齢は",age, "才で身長は",height,"cmで体重は",weight,"kgですね。")
+# 眠いので今すぐにでも寝たいです。
